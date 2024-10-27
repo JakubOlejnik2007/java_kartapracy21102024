ddf48062cd5da51f445f5eda0216a0b90cf9efba
JakubOlejnik2007 <jacobole2000@gmail.com>
2024-10-27 22:47:36 +0100

    Created Kadry.java

---
+++ src/Kadry.java
@@ -0,0 +1,10 @@
+public class Kadry {
+    private Pracownik[] pracownicy_;
+    private int zatrudnienie_;
+
+    public Kadry() {
+        this.pracownicy_ = new Pracownik[100];
+        this.zatrudnienie_ = 0;
+    }
+
+}
