7de6381a815e110b7659b0244890731bf1427496
JakubOlejnik2007 <jacobole2000@gmail.com>
2024-10-27 22:52:49 +0100

    Kadry.dodajPracownika and Kadry.dodajPracownikaInteraktywnie

--- src/Kadry.java
+++ src/Kadry.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Kadry {
     private Pracownik[] pracownicy_;
     private int zatrudnienie_;
@@ -7,4 +9,38 @@ public class Kadry {
         this.zatrudnienie_ = 0;
     }
 
+    void dodajPracownika(Pracownik pracownik) {
+        if (this.zatrudnienie_ < 100) {
+            this.pracownicy_[this.zatrudnienie_++] = pracownik;
+            System.out.println("Dodano pracownika");
+            return;
+        }
+        System.out.println("Nie można dodać pracownika");
+    }
+
+    void dodajPracownikaInteraktywnie() {
+        if (this.zatrudnienie_ < 100) {
+            Scanner sc = new Scanner(System.in);
+
+            System.out.println("Wprowadź imię:\t");
+            String imie = sc.nextLine();
+
+            System.out.println("Wprowadź nazwisko:\t");
+            String nazwisko = sc.nextLine();
+
+            System.out.println("Wprowadź placę:\t");
+            double placa = sc.nextDouble();
+
+            System.out.println("Wprowadź plec:\t");
+            char plec = sc.next().charAt(0);
+
+            System.out.println("Wprowadź imię:\t");
+            int dzial = sc.nextInt();
+
+            this.dodajPracownika(new Pracownik(imie, nazwisko, placa, plec, dzial));
+
+            return;
+        }
+        System.out.println("Nie można dodać pracownika");
+    }
 }
